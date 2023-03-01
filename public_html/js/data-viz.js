dea5ff7411631d03238cc2660e6b0af6a9a8d4d1
Naoru347 <joshua.paiz@gmail.com>
2023-03-01 12:35:15 -0500

    Got chart addition working. Reformatted JSON files

--- public_html/js/data-viz.js
+++ public_html/js/data-viz.js
@@ -1,18 +1,46 @@
-async function getDataAA(file) {
+async function getData1(file) {
     let holder = await fetch('../jsonData/data1.json');
-    let data = await holder.json();
+    const data = await holder.json();
     console.log(data);
+    createTable(data);
 }
-function getData() {
-    let file = "./jsonData/data3.json"
-    fetch(file, {method: "GET", mode: 'cors', headers: {'Content-Type': 'application/json',}})
-    .then(response => response.json())
-    .then(data => console.log(data));
+async function getData2(file) {
+    let holder = await fetch('../jsonData/data2.json');
+    const data = await holder.json();
+    console.log(data);
+    createTable(data);
 }
 
-function test3(){
-    console.log("test was successful from location 3");
-    var el = document.getElementById("testContent");
-    el.textContent = "teset test was successful from location three, printing to first test space."
-    
+async function getData3(file){
+    let holder = await fetch('../jsonData/data3.json');
+    const data = await holder.json();
+    console.log(data);
+    createTable(data);
 }
+    
+function createTable(data){
+    var table = document.createElement('table'),
+    tr = document.createElement('tr'),
+    rows = [],
+    max = 0,
+    i;
+
+table.appendChild(tr);
+data.forEach(function (o) {
+    var th = document.createElement('th');
+    th.appendChild(document.createTextNode(o.Header));
+    tr.appendChild(th);
+    max = Math.max(max, o.Values.length);
+});
+for (i = 0; i < max; i++) {
+    tr = document.createElement('tr'),        
+    table.appendChild(tr);
+    data.forEach(function (o) {
+        var td = document.createElement('td');
+        tr.appendChild(td);
+        td.appendChild(document.createTextNode(i in o.Values ? o.Values[i] : ''));
+    });
+}
+document.body.appendChild(table);
+}
+
