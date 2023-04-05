18f66f645d186496eb50faff53c6529fea41c966
Naoru347 <joshua.paiz@gmail.com>
2023-04-05 10:54:37 -0400

    Wrapped up for the client

--- public_html/js/data-viz.js
+++ public_html/js/data-viz.js
@@ -42,5 +42,49 @@ for (i = 0; i < max; i++) {
     });
 }
 document.body.appendChild(table);
+
+var histogramCanvas = document.createElement('canvas');
+document.body.appendChild(histogramCanvas);
+
+createHistogram(data, histogramCanvas);
 }
 
+function createHistogram(data, histogramCanvas) {
+    const labels = data.map(item => item.Header);
+    const maxDataLength = data.reduce((max, item) => Math.max(max, item.Values.length), 0);
+    
+    const datasets = Array.from({ length: maxDataLength }, (_, rowIndex) => {
+        const rowData = data.map(item => item.Values[rowIndex] || 0);
+        const color = {
+            r: Math.floor(Math.random() * 256),
+            g: Math.floor(Math.random() * 256),
+            b: Math.floor(Math.random() * 256)
+        };
+
+        return {
+            label: `Row ${rowIndex + 1}`,
+            data: rowData,
+            backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, 0.5)`,
+            borderColor: `rgba(${color.r}, ${color.g}, ${color.b}, 1)`,
+            borderWidth: 1
+        };
+    });
+
+    new Chart(histogramCanvas, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: datasets,
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+            indexAxis: 'y',
+            responsive: true,
+            maintainAspectRatio: false,
+        },
+    });
+}
